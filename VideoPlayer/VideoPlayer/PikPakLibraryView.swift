1740d409fc28752011165d0068e5204c4f5172ed
Mortaza Minoz ~ <secretcampusup@gmail.com>
2026-08-11 02:09:10 +0300

    Update PikPakLibraryView.swift

--- VideoPlayer/VideoPlayer/PikPakLibraryView.swift
+++ VideoPlayer/VideoPlayer/PikPakLibraryView.swift
@@ -218,7 +218,7 @@ struct PikPakLibraryView: View {
                     url: url,
                     title: file.name,
                     resumeAt: vm.nowPlayingResumeAt,
-                    linkId: vm.nowPlayingLinkId,
+                    linkId: vm.nowPlayingLinkId
                 ) { seconds, duration, w, h in
                     vm.updatePlaybackProgress(
                         seconds: seconds,
